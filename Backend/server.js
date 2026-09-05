36974b765183a502e5d25a347cc26d6ac4ee4d21
AbdElrahman Sobhy <abdelrhman9048@gmail.com>
2026-09-05 10:48:45 +0300

    Setup backend environment, database connection, and dependencies

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from "./config/db.js"
+
+dotenv.config()
+connectDB()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT , ()=> console.log(`Server running on port ${PORT}`))
